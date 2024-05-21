fb9f7255bfa9053050045ad8e9d04a19256d789d
Sajjad Abdullah <clashking1545@gmail.com>
2024-05-21 20:29:07 +0600

    Mui initialized

---
+++ src/app/(withCommonLayout)/layout.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<>
+			<Box className="min-h-screen">{children}</Box>
+		</>
+	);
+};
+
+export default CommonLayout;
---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const Home = () => {
+	return (
+		<div>
+			<h1>This is Home component</h1>
+			<Button> Test Button </Button>
+		</div>
+	);
+};
+
+export default Home;
