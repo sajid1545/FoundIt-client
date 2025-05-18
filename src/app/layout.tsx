065b835b520462d14b514e597241181ff4f323df
Sajjad Abdullah <clashking1545@gmail.com>
2025-05-18 21:20:42 +0600

    Adds Vercel analytics
    Implements Vercel Analytics to track user behavior and gain insights into application usage.
    This helps to better understand user interactions and improve the application based on real-world data.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,35 @@
 import Providers from "@/lib/Providers/Providers";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "FoundIt",
-	description: "Lost and Found Items Management System",
+  title: "FoundIt",
+  description: "Lost and Found Items Management System",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<Providers>
-			<html lang="en">
-				<body className={inter.className}>
-					<AppRouterCacheProvider>
-						<>
-							<Toaster position="top-center" richColors closeButton />
-							{children}
-						</>
-					</AppRouterCacheProvider>
-				</body>
-			</html>
-		</Providers>
-	);
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Providers>
+      <html lang="en">
+        <body className={inter.className}>
+          <AppRouterCacheProvider>
+            <>
+              <Toaster position="top-center" richColors closeButton />
+              {children}
+              <Analytics />
+            </>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </Providers>
+  );
 }
